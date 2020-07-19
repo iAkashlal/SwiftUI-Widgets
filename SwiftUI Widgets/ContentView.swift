c78d26acc00d04876fe94a1e60436aa3b881d8c3
Akashlal <akashlal@heady.io>
2020-07-19 08:18:14 +0530

    Initial commit

---
+++ SwiftUI Widgets/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI Widgets
+//
+//  Created by Akashlal Bathe on 15/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Widgets/SwiftUI_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WidgetsApp.swift
+//  SwiftUI Widgets
+//
+//  Created by Akashlal Bathe on 15/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
